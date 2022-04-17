4c4480025247fe9fc4eae083b63df4317d163995
juneau <juneaugawat@hotmail.com>
2022-04-17 00:13:23 -0400

    updated map

--- Assets/Scripts/BallAgent.cs
+++ Assets/Scripts/BallAgent.cs
@@ -6,10 +6,12 @@ using Unity.MLAgents.Actuators;
 
 public class BallAgent : Agent
 {
+    public GameObject parent;
     public Transform Target;
     public float forceMultiplier = 10;
     bool boom;
     Rigidbody rBody;
+    public List<GameObject> planes;
     public Transform s1;
     public Transform s2;
     public Transform s3;
@@ -39,6 +41,107 @@ public class BallAgent : Agent
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
+        SetWallsAndSpawns();
+        for(int i = 0; i < 7; i++) {
+            planes.Add(parent.transform.Find($"Plane ({i})").gameObject);
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        int spawn = Random.Range(1, 7);
+        ResetSpawn();
+
+        foreach(GameObject plane in planes) {
+            int colour = Random.Range(1, 3);
+            if(colour == 1) {
+                plane.GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+            else {
+                plane.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+
+        // Move the target to a new spot
+        spawn = Random.Range(1, 12);
+        for(int i = 1; i <= 12; i++) {
+            if(spawn == i) {
+                Target.localPosition = spawns[i-1].localPosition;
+            }
+        }
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition); // 3
+        sensor.AddObservation(this.transform.localPosition); // 3
+        foreach(Transform t in walls) { // 36
+            sensor.AddObservation(t.localPosition);
+        }
+        foreach(GameObject g in planes) {
+            sensor.AddObservation(g.transform.localPosition); // 21
+        }
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x); // 1
+        sensor.AddObservation(rBody.velocity.z); // 1
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        AddReward(-1f / MaxStep);
+
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+
+        // Fell off platform
+        if(this.transform.localPosition.y < 0) {
+            SetReward(-.1f);
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut) {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("platforms") && other.gameObject.GetComponent<MeshRenderer>().material.color == Color.red) {
+            SetReward(-.1f);
+            EndEpisode();
+        }
+        //else if(other.gameObject.CompareTag("platforms") && other.gameObject.GetComponent<MeshRenderer>().material.color == Color.green) {
+        //    SetReward(.1f);
+        //}
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("target")) {
+            SetReward(1f);
+            EndEpisode();
+        }
+        //else if(other.gameObject.CompareTag("wall")) {
+        //    //Debug.Log("Touched wall");
+        //    //SetReward(-.1f);
+        //    EndEpisode();
+        //}
+    }
+
+    private void ResetSpawn()
+    {
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        int spawn = Random.Range(0, 11);
+        this.transform.localPosition = spawns[spawn].localPosition;
+    }
+
+    private void SetWallsAndSpawns() {
         walls[0] = w1;
         walls[1] = w2;
         walls[2] = w3;
@@ -63,142 +166,5 @@ public class BallAgent : Agent
         spawns[9] = s10;
         spawns[10] = s11;
         spawns[11] = s12;
-    }
-
-    public override void OnEpisodeBegin()
-    {
-        int spawn = Random.Range(1, 7);
-        // If the Agent fell, zero its momentum
-        //if(this.transform.localPosition.y < 0) {
-        //    this.rBody.angularVelocity = Vector3.zero;
-        //    this.rBody.velocity = Vector3.zero;
-        //    //if(spawn == 1)
-        //    //    this.transform.localPosition = s5.localPosition;
-        //    //else if(spawn == 2)
-        //    //    this.transform.localPosition = s1.localPosition;
-        //    //else if(spawn == 3)
-        //    //    this.transform.localPosition = s2.localPosition;
-        //    //else
-        //    //    this.transform.localPosition = new Vector3(0, 0.5f, 0);
-        //    ResetSpawn();
-        //}
-        //else
-        //{
-        //    ResetSpawn();
-        //}
-
-        ResetSpawn();
-
-        // Move the target to a new spot
-        spawn = Random.Range(1, 12);
-        if (spawn == 1)
-            Target.localPosition = s1.localPosition;
-        if (spawn == 2)
-            Target.localPosition = s2.localPosition;
-        if (spawn == 3)
-            Target.localPosition = s3.localPosition;
-        if (spawn == 4)
-            Target.localPosition = s4.localPosition;
-        if (spawn == 5)
-            Target.localPosition = s5.localPosition;
-        if (spawn == 6)
-            Target.localPosition = s6.localPosition;
-        if (spawn == 7)
-            Target.localPosition = s7.localPosition;
-        if (spawn == 8)
-            Target.localPosition = s8.localPosition;
-        if (spawn == 9)
-            Target.localPosition = s9.localPosition;
-        if (spawn == 10)
-            Target.localPosition = s10.localPosition;
-        if (spawn == 11)
-            Target.localPosition = s11.localPosition;
-        if (spawn == 12)
-            Target.localPosition = s12.localPosition;
-    }
-
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        // Target and Agent positions
-        sensor.AddObservation(Target.localPosition);
-        sensor.AddObservation(this.transform.localPosition);
-        sensor.AddObservation(w1.localPosition);
-        sensor.AddObservation(w2.localPosition);
-        sensor.AddObservation(w3.localPosition);
-        sensor.AddObservation(w4.localPosition);
-        sensor.AddObservation(w5.localPosition);
-        sensor.AddObservation(w6.localPosition);
-        sensor.AddObservation(w7.localPosition);
-        sensor.AddObservation(w8.localPosition);
-        sensor.AddObservation(w9.localPosition);
-        sensor.AddObservation(w10.localPosition);
-        sensor.AddObservation(w11.localPosition);
-        sensor.AddObservation(w12.localPosition);
-
-        // Agent velocity
-        sensor.AddObservation(rBody.velocity.x);
-        sensor.AddObservation(rBody.velocity.z);
-    }
-
-    public override void OnActionReceived(ActionBuffers actionBuffers)
-    {
-        // Actions, size = 2
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-        controlSignal.z = actionBuffers.ContinuousActions[1];
-        rBody.AddForce(controlSignal * forceMultiplier);
-
-        // Rewards
-        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
-        //float dtw1 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw2 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw3 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw4 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw5 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw6 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw7 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw8 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw9 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw10 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw11 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-        //float dtw12 = Vector3.Distance(this.transform.localPosition, w1.localPosition);
-
-        // Reached target
-        if (distanceToTarget < 1.42f)
-        {
-            SetReward(1.0f);
-            EndEpisode();
-        }
-
-        // Fell off platform
-        else if (this.transform.localPosition.y < 0)
-        {
-            EndEpisode();
-        }
-    }
-
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Finish"))
-        {
-            //Debug.Log("Touched wall");
-            AddReward(-0.2f);
-            EndEpisode();
-        }
-    }
-
-    private void ResetSpawn()
-    {
-        this.rBody.angularVelocity = Vector3.zero;
-        this.rBody.velocity = Vector3.zero;
-        int spawn = Random.Range(0, 11);
-        this.transform.localPosition = spawns[spawn].localPosition;
     }
 }
